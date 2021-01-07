2102c98de18026ff4f8eab8496eea6dc4fb8cab3
Andrew Wolf <wolfpassion20@gmail.com>
2021-01-07 16:34:05 -0500

    Added all the data fields as in the models, except replaced models with forms.

--- commoninfo/forms.py
+++ commoninfo/forms.py
@@ -1,4 +1,7 @@
 from django import forms 
 
 class AddPatient(forms.Form):
-    pass
+    unique_ID = forms.CharField(max_length=20, unique=True, primary_key = True, help_text="Enter the patient's unique ID: ")
+    first_Name = forms.CharField(max_length=20, help_text="Enter the patient's first name: ")
+    last_Name = forms.CharField(max_length=20, help_text="Enter the patient's last name: ")
+    date_of_birth = forms.DateField(help_text="Enter the patient's date of birth: ")
