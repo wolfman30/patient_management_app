5f165893af4c2392e56ad7b37f008e182cb8e5b7
Andrew Wolf <wolfpassion20@gmail.com>
2021-01-07 17:01:03 -0500

    Changed years in datetime.timedelta to weeks= 52 * 130 since this methods does not take years as an argument.

--- commoninfo/forms.py
+++ commoninfo/forms.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 class AddPatient(forms.Form):
+
     unique_ID = forms.CharField(max_length=20, unique=True, primary_key = True, help_text="Enter the patient's unique ID: ")
     first_Name = forms.CharField(max_length=20, help_text="Enter the patient's first name: ")
     last_Name = forms.CharField(max_length=20, help_text="Enter the patient's last name: ")
@@ -15,7 +16,9 @@ class AddPatient(forms.Form):
         if dob > datetime.date.today():
             raise ValidationError(_('The patient cannot be born in the future.'))
 
-        if dob < datetime.date.today() - datetime.timedelta(years=130):
+        if dob < datetime.date.today() - datetime.timedelta(weeks=52*130): #52 weeks in a year, 130 years 
             raise ValidationError(_('The date of birth entered makes the patient impossibly old.'))
 
         return dob
+
+
