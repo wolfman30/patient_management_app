a9fb47bdb78cf2ee0656b9fa274706e7b50df146
Andrew Wolf <wolfpassion20@gmail.com>
2021-01-11 04:51:40 -0500

    Added default value of null to reason_for_visit in Patient model

--- commoninfo/models.py
+++ commoninfo/models.py
@@ -6,14 +6,14 @@ class Patient(models.Model):
     first_Name = models.CharField(max_length=20, help_text="Enter the patient's first name: ")
     last_Name = models.CharField(max_length=20, help_text="Enter the patient's last name: ")
     date_of_birth = models.DateField(help_text="Enter the patient's date of birth: ")
-    reason_for_visit = models.TextField(max_length = 400)
+    reason_for_visit = models.TextField(max_length = 300, default = 'null', help_text='Enter the reason for visit', )
 
     class Meta: 
-        ordering = ['unique_ID', 'first_Name', 'last_Name', 'date_of_birth']
+        ordering = ['unique_ID', 'first_Name', 'last_Name', 'date_of_birth', 'reason_for_visit']
 
     def get_absolute_url(self): 
         """Returns the url to access a particular instance of Patient"""
         return reverse('patient-detail', args=[str(self.primary_key)])
 
     def __str__(self): 
-        return f'{self.unique_ID}, {self.first_Name}, {self.last_Name}, {self.date_of_birth}'
+        return f'{self.unique_ID}, {self.first_Name}, {self.last_Name}, {self.date_of_birth}, {self.reason_for_visit}'
