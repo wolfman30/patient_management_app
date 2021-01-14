4a93ae1b19d64debdaee49315c1ae1be07d0741f
Andrew Wolf <wolfpassion20@gmail.com>
2021-01-14 12:47:36 -0500

    Added code to successfully link the css file to templates

--- patientManagementApp/settings.py
+++ patientManagementApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'patientManagementApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['commoninfo'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
+STATIC_ROOT = PROJECT_DIR + '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static", '/css/')
+]
+
--- patientManagementApp/urls.py
+++ patientManagementApp/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
    path('commoninfo/', include('commoninfo.urls')),
    path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
