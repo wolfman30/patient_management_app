26d049e10e3269508040ce4af6718bf85b925b79
Andrew Wolf <wolfpassion20@gmail.com>
2021-01-19 22:26:46 -0500

    Working on validator for birth date to make sure the date is not in the future nor too far in the past. For now, the browswer displays a TypeError.

--- commoninfo/models.py
+++ commoninfo/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
+#code from https://stackoverflow.com/questions/5013041/how-to-restrict-user-to-select-date-between-range-of-years-in-django by Ori
+from django.core.exceptions import ValidationError 
+
 alpha = RegexValidator(r'^[a-zA-Z]*$', 'Only letters are allowed in names.')
 nums = RegexValidator(r'^[0-9]*$', 'Phone number should have only numbers.')
 #created the above variable based on Stack Overflow author Martijn Pieters from the URL below
 #https://stackoverflow.com/questions/17165147/how-can-i-make-a-django-form-field-contain-only-alphanumeric-characters
 
 
+def validate_birthyear(value):
+    '''
+    code from https://stackoverflow.com/questions/5013041/how-to-restrict-user-to-select-date-between-range-of-years-in-django by Ori
+    '''
+    if value < 1910 or value > 2021: 
+        raise ValidationError(f'{value} is not a valid birth year!')
 
 class Patient(models.Model): 
     unique_ID = models.CharField(max_length=20, unique=True, primary_key = True, help_text="Enter the patient's unique ID: ")
     first_Name = models.CharField(max_length=20, validators=[alpha], help_text="Enter the patient's first name: ")
     last_Name = models.CharField(max_length=20, validators=[alpha], help_text="Enter the patient's last name: ")
-    date_of_birth = models.DateField(help_text="Enter the patient's date of birth in the form of year-month-day")
+    date_of_birth = models.DateField(validators=[validate_birthyear], help_text="Enter the patient's date of birth in the form of year-month-day")
     address = models.CharField(max_length=40, help_text = 'Enter address of residence')
     phone = models.CharField(max_length = 12, validators=[nums], help_text="Enter the patient's phone number")
     email = models.EmailField(max_length = 40, blank='True', help_text = "Enter the patient's email address: ")
