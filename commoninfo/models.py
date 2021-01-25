9df190844cf71512a2e55487ec28a79fdf811049
Andrew Wolf <wolfpassion20@gmail.com>
2021-01-25 13:32:36 -0500

    Added new fields for patients' address for better database design

--- commoninfo/models.py
+++ commoninfo/models.py
@@ -25,10 +25,13 @@ class Patient(models.Model):
     first_Name = models.CharField(max_length=20, validators=[alpha], help_text="Enter the patient's first name: ")
     last_Name = models.CharField(max_length=20, validators=[alpha], help_text="Enter the patient's last name: ")
     date_of_birth = models.DateField(validators=[validate_birthyear], help_text="Enter the patient's date of birth in the form of year-month-day")
-    address = models.CharField(max_length=40, help_text = 'Enter address of residence')
+    address = models.CharField(max_length=40, help_text = 'Enter your street address')
+    city = models.CharField(default="null", max_length=20, help_text = "Enter your city of residence")
+    state = models.CharField(default = "null", max_length=20, help_text = "Enter your state of residence.")
+    zip_code = models.PositiveIntegerField(default = 1, help_text = 'Enter your zipcode')
     phone = models.CharField(max_length = 12, validators=[nums], help_text="Enter the patient's phone number")
     email = models.EmailField(max_length = 40, blank='True', help_text = "Enter the patient's email address: ")
-    reason_for_visit = models.TextField(max_length = 300, help_text='Enter the reason for visit', )
+    reason_for_visit = models.TextField(max_length = 300, help_text='Enter the reason for visit')
 
     class Meta: 
         ordering = ['unique_ID', 'first_Name', 'last_Name', 'date_of_birth', 'address', 'phone', 'email', 'reason_for_visit']
